c7d840a095bb86ee075efd89f5c765a9102d6f1b
yjLeo <50327581+yijunL@users.noreply.github.com>
2023-08-31 18:20:03 +0800

    Delete codes/VAE-CNN directory

--- codes/VAE-CNN/download_pretrain.sh
+++
@@ -1,3 +0,0 @@
-mkdir pretrain
-wget -P pretrain https://thunlp.oss-cn-qingdao.aliyuncs.com/opennre/pretrain/glove/glove.6B.50d_mat.npy
-wget -P pretrain https://thunlp.oss-cn-qingdao.aliyuncs.com/opennre/pretrain/glove/glove.6B.50d_word2id.json
