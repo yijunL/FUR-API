56287a6f2a6660db10aa96c0b8631452a8f7fb78
yjLeo <50327581+yijunL@users.noreply.github.com>
2023-09-04 15:41:16 +0800

    Update main.py

--- codes/GANomaly-BERT/main.py
+++ codes/GANomaly-BERT/main.py
@@ -18,8 +18,8 @@ parser.add_argument("--data_dir", type=str, default="./data", help="training dat
 parser.add_argument("--seed", type=int, default=42, help="random seed for initialization")
 args = parser.parse_args()
 
-# 损失函数
-reconstruction_criterion = nn.MSELoss()  # 均方误差损失
+
+reconstruction_criterion = nn.MSELoss()  
 
 
 def set_seed(seed):
@@ -32,24 +32,24 @@ def set_seed(seed):
 def train(model, config, train_iterator, dev_iterator):
     model.train()
     start_time = time.time()
-    # 优化器
+
     optimizer = torch.optim.Adam(model.parameters(), lr=config.learning_rate)
     t_total = len(train_iterator) * config.num_epochs
     break_flag = False
     total_batch = 0
     last_improve = 0
     best_dev_loss = float('inf')
-    # 训练循环
+
     for epoch in range(config.num_epochs):
         print("Epoch [{}/{}]".format(epoch + 1, config.num_epochs))
         with open("logger.txt", "a+", encoding="utf-8") as fw:
             fw.write("Epoch [{}/{}]".format(epoch + 1, config.num_epochs))
-        # 开始训练
+
         for _, (batch_data, labels) in enumerate(train_iterator):
             cls_vector, x1, z1, z2, z3, z4 = model(
                 input_ids=batch_data['input_ids'],
                 attention_mask=batch_data['attention_mask'])
-            # 计算重构损失
+
             reconstruction_loss1 = reconstruction_criterion(cls_vector, x1)
             reconstruction_loss2 = reconstruction_criterion(z1, z2)
             reconstruction_loss3 = reconstruction_criterion(z3, z4)
@@ -57,7 +57,7 @@ def train(model, config, train_iterator, dev_iterator):
             loss = reconstruction_loss1+reconstruction_loss2+reconstruction_loss3
             loss.backward()
             
-            # 更新模型参数
+
             optimizer.step()
             optimizer.zero_grad()
 
@@ -95,13 +95,13 @@ def eval(model, iterator):
             cls_vector, x1, z1, z2, z3, z4 = model(
                 input_ids=batch["input_ids"],
                 attention_mask=batch["attention_mask"])
-            # 计算重构损失
+
             reconstruction_loss1 = reconstruction_criterion(cls_vector, x1)
             reconstruction_loss2 = reconstruction_criterion(z1, z2)
             reconstruction_loss3 = reconstruction_criterion(z3, z4)
             
             loss = reconstruction_loss1+reconstruction_loss2+reconstruction_loss3
-            # 计算重构损失
+
             re_loss = re_loss + loss
         re_loss = re_loss / len(iterator)
     return re_loss
@@ -115,7 +115,7 @@ def threshold(model, config, iterator,auto_rate = 0.25):
             cls_vector, x1, z1, z2, z3, z4 = model(
                 input_ids=batch["input_ids"],
                 attention_mask=batch["attention_mask"])
-            # 计算重构损失
+
             loss = reconstruction_criterion(cls_vector, x1)
             loss_set.append(loss.item())
     
@@ -124,7 +124,7 @@ def threshold(model, config, iterator,auto_rate = 0.25):
     return loss_set[int(len(loss_set)*auto_rate)]
 
 def test(model, config, iterator, flag=0.5):
-    # 在测试集上进行预测和评估
+
     print("testing...")
     model.eval()
     model.load_state_dict(torch.load(config.saved_model))
@@ -135,7 +135,7 @@ def test(model, config, iterator, flag=0.5):
         cls_vector, x1, z1, z2, z3, z4 = model(
             input_ids=batch["input_ids"],
             attention_mask=batch["attention_mask"])
-        # 计算重构损失
+
         reconstruction_loss = reconstruction_criterion(cls_vector, x1)
 #         print("test reconstruction_loss: ", reconstruction_loss.item())
         with open("logger.txt", "a+", encoding="utf-8") as fw:
