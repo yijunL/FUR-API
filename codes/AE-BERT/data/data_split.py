96f5c87f1d9eafe493d8a378fae5824a2825c34b
yjLeo <50327581+yijunL@users.noreply.github.com>
2023-09-05 14:59:56 +0800

    Update data_split.py

--- codes/AE-BERT/data/data_split.py
+++ codes/AE-BERT/data/data_split.py
@@ -10,17 +10,17 @@ with open('train.csv', 'r', errors='ignore') as file:
     for row in csv_reader:
         data.append(row)
         
-    header = [data[0]]
-    data = data[1:]
-    
-    random.shuffle(data)
-    nor = []
-    ano = []
-    for i in data:
-        if(i[-1]=="normal"):
-            nor.append(i)
-        else:
-            ano.append(i)
+header = [data[0]]
+data = data[1:]
+
+random.shuffle(data)
+nor = []
+ano = []
+for i in data:
+    if(i[-1]=="normal"):
+        nor.append(i)
+    else:
+        ano.append(i)
 
 with open('train_n.csv', 'w', newline='') as file:
     writer = csv.writer(file)
